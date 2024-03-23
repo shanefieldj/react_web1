1768e122e46f335eccf8c4d782cbd5fcc6b705d6
shanefieldj <shanefieldjacob@gmail.com>
2024-03-23 18:31:13 -0400

    header add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import header1 from './components/header1';
 
 function App() {
   const value = 'World';
+
+  <header1 />
   return <div>Hello there {value}</div>;
 }
 
