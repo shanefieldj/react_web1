2cf359310b7a004e13da142f0cc0fbaee8c21793
shanefieldj <shanefieldjacob@gmail.com>
2024-03-26 21:37:17 -0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
+  return (
+    <div>
+      <Header />
+      {/* Other content of your application */}
+    </div>
+  );
+  /*
   const value = 'World';
   return <div>Hello {value}</div>;
+  */
 }
 
 export default App;
